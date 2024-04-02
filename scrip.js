afc79901cf3c644f9ffa9d24f8ea8860542e1298
Milica2809 <milicajegic71@gmail.com>
2024-04-02 13:22:16 +0200

    praksa gotovo

---
+++ scrip.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  
+});
+
+
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
